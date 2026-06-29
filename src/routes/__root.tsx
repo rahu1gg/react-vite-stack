ae0ebf1c31546a04ee94c70ceabef42cfdbe345c
Rahul Palamarthi <rahulpalamarthi@gmail.com>
2026-06-29 22:37:46 +0530

    refactor: react-scan and other tools

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,7 +2,6 @@ import type { QueryClient } from '@tanstack/react-query';
 import { createRootRouteWithContext, Outlet } from '@tanstack/react-router';
 import React from 'react';
 import { Fragment } from 'react/jsx-runtime';
-import { scan } from 'react-scan';
 import { QueryProvider } from '@/client/providers/query-provider';
 import { TanStackRouterDevtools } from '@/client/providers/router-devtools';
 import { ThemeProvider } from '@/client/providers/theme-provider';
@@ -12,14 +11,10 @@ interface MyRouterContext {
   queryClient: QueryClient;
 }
 
-function scanReactApp() {
-  scan({
-    enabled: true,
-    log: true, // logs render info to console (default: false)
-  });
+if (import.meta.env.MODE === 'development') {
+  const { scanReactApp } = await import('@/features/shared/utils/react-scan');
+  scanReactApp();
 }
-
-import.meta.env.MODE === 'development' && scanReactApp();
 
 export const Route = createRootRouteWithContext<MyRouterContext>()({
   component: RootRouteComponent,
